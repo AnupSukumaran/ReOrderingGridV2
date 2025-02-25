1e8b16b313da9238ab974e84fb8caf38a5b1e1b2
AnupSukumaran <anup.sukumaran9@gmail.com>
2025-02-25 15:41:36 +0000

    - completed

--- ReOrderingGridV2/ContentView.swift
+++ ReOrderingGridV2/ContentView.swift
@@ -10,17 +10,45 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var colors: [Color] = [.red, .blue, .purple, .yellow, . black, .indigo, .cyan, .brown, .mint, .orange]
-    
+    @State private var draggingItem: Color?
     var body: some View {
         NavigationStack {
             ScrollView(.vertical) {
                 let columns = Array(repeating: GridItem(spacing: 10), count: 3)
                 LazyVGrid(columns: columns, spacing: 10) {
                     ForEach(colors, id: \.self) { color in
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(color.gradient)
-                            .frame(height: 100)
+                        GeometryReader {
+                            let size = $0.size
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(color.gradient)
+                                /// Drag
+                                .draggable(color) {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(.ultraThinMaterial)
+                                        .frame(width: 1, height: 1)
+                                        .onAppear {
+                                            draggingItem = color
+                                        }
+                                }
+                                /// Drop
+                                .dropDestination(for: Color.self) { items, location in
+                                    draggingItem = nil
+                                    return false
+                                } isTargeted: { status in
+                                    if let draggingItem, status, draggingItem != color {
+                                        // Moving color from source to destination
+                                        if let sourceIndex = colors.firstIndex(of: draggingItem), let destinationIndex = colors.firstIndex(of: color) {
+                                            withAnimation {
+                                                let sourceItem = colors.remove(at: sourceIndex)
+                                                colors.insert(sourceItem, at: destinationIndex)
+                                            }
+                                        }
+                                    }
+                                }
+                        }
+                        .frame(height: 100)
                     }
+                   
                     
                 }
                 .padding(15)
