5103b25d9b0beee1028bbeb3b4b0e1e603dfecd3
AnupSukumaran <anup.sukumaran9@gmail.com>
2025-02-25 14:51:36 +0000

    Initial Commit

---
+++ ReOrderingGridV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReOrderingGridV2
+//
+//  Created by Sukumar.Sukumaran on 25/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReOrderingGridV2/ReOrderingGridV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ReOrderingGridV2App.swift
+//  ReOrderingGridV2
+//
+//  Created by Sukumar.Sukumaran on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReOrderingGridV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
